c4295d28088260a5cb000de358ff3855b3ac74c4
MAZTRO <designe.mallet@gmail.com>
2020-01-03 10:25:29 -0500

    Fix Syntax

--- fun1.c
+++ fun1.c
@@ -1,18 +1,17 @@
 #include "monty.h"
+
 /**
   * push - Function that add an item to stack
   * @stack : pointer of input
   * @item: int of input
   **/
+
 void push(stack_t **stack, unsigned int value)
 {
 	stack_t *aux;
 
 	aux = malloc(sizeof(aux));
-	if (aux == NULL)
-	{
-		return (EXIT_FAILURE);
-	}
+	if (aux == NULL) return (EXIT_FAILURE);
 	if (*stack == NULL)
 	{
 		*stack = aux;
@@ -24,59 +23,73 @@ void push(stack_t **stack, unsigned int value)
 	{
 		aux->next = *stack;
 		(*stack)->prev = aux;
-		aux->prev = NULL
+		aux->prev = NULL;
 		aux->n = value;
 		*stack = aux;
 	}
 }
-{/**
-  * push - Function that print date in the screen
-  * @stack : pointer of input
-  * @item: int of input
-  **/
+
+/**
+ * pall - Function that print date in the screen
+ * @stack : pointer of input
+ * @item: int of input
+ */
+
 void pall(stack_t **stack, unsigned int value)
+{
 	size_t count = 0;
 
+	if (!stack) return(EXIT_SUCCESS);
 	for (count = 0; *stack != NULL; count++)
 	{
-		printf("%d\n", *stack->n);
-		*stack = *stack->next;
+		printf("%d\n", (*stack)->n);
+		*stack = (*stack)->next;
 	}
 }
+
 /**
-  * pint - Function that print date in the screen
-  * @stack : pointer of input
-  * @item: int of input
-  **/
+ * pint - Function that print date in the screen
+ * @stack : pointer of input
+ * @item: int of input
+ */
+
 void pint(stack_t **stack, unsigned int value)
 {
-	printf("%d\n", *stack->n);
-	*stack = *stack->next;
+	if (!stack)
+	{
+		fprintf(stderr, "L%lu: can't pint, stack empty\n", value);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", (*stack)->n);
 }
+
 /**
-  * pop - Function that remove the top element of the stack
-  * @stack : pointer of input
-  * @item: int of input
-  **/
+ * pop - Function that remove the top element of the stack
+ * @stack : pointer of input
+ * @item: int of input
+ */
+
 void pop(stack_t **stack, unsigned int value)
 {
 	stack_t *aux;
 
-	if (*stack == NULL)
-		printf("L<%lu>: can't pop an empty stack\n", value);
+	if (stack == NULL)
+		fprintf(stderr, "L%lu: can't pop an empty stack\n", value);
 		exit(EXIT_FAILURE);
 	aux = *stack;
 	*stack = aux->next;
 	aux->next = NULL;
-	stack->prev = NULL;
-	free(aux)
+	(*stack)->prev = NULL;
+	free(aux);
 }
+
 /**
-  * swap - Function that print date in the screen
-  * @stack : pointer of input
-  * @item: int of input
-  **/
-void pall(stack_t **stack, unsigned int value)
+ * swap - Function that print date in the screen
+ * @stack : pointer of input
+ * @item: int of input
+ */
+
+void swap(stack_t **stack, unsigned int value)
 {
 	stack_t *aux;
 	int temp = 0;
